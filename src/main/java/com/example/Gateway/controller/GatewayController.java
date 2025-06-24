40e3466071cc53d07bc511da3bd456521c91e697
vetrivel1411 <vetrivelifet22@gmail.com>
2025-06-24 12:22:41 +0530

    code

---
+++ src/main/java/com/example/Gateway/controller/GatewayController.java
@@ -0,0 +1,31 @@
+package com.example.Gateway.controller;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class GatewayController {
+
+    private final RestTemplate restTemplate;
+
+    public GatewayController(RestTemplateBuilder builder) {
+        this.restTemplate = builder.build();
+    }
+
+    @GetMapping("/gateway")
+    public String getGatewayResponse() {
+        return "Welcome to Gateway.." ;
+    }
+
+    @GetMapping("/gateway/IAM")
+    public String getIAMResponse() {
+        return restTemplate.getForObject("http://localhost:8080/IAM", String.class);
+    }
+
+    @GetMapping("/gateway/rendering")
+    public String getRenderingResponse() {
+        return restTemplate.getForObject("http://localhost:8081/rendering", String.class);
+    }
+}
